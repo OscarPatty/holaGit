1b90c67a334f67c96403ce2af498858b4b69a25d
oscarpatty <oscarpatty@gmail.com>
2025-02-24 00:59:48 -0400

    Primer commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="message-box">
+      <h1>Hola Git</h1>
+      Bienvenidos !
+    </div>
+
+  </div>
+
+</body>
+
+</html>
